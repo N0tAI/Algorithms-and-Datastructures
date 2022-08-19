96f0012fa5d6ab8a4daabd1f8549435568841b4b
Not_AI <44149574+N0tAI@users.noreply.github.com>
2022-08-19 00:32:29 -0400

    Began Binary Trees

---
+++ trees/binary_tree_array.c
@@ -0,0 +1,6 @@
+// Arrays work best for Complete binary trees (binary trees where all nodes at a height less than height(tree) are full (all nodes are degree(2)) and the nodes at height(tree) are added
+// leftmost to right most
+// 
+// ie this would mean that an array is best suited for a binary tree when the binary tree will NOT put any holes in the array storing the nodes
+
+
